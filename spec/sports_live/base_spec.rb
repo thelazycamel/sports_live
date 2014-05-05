d7e1c330cd7ae7700e016a74c13a6d222a91701f
Stuart Hanscombe <stuart.hanscombe@betvictor.com>
2014-05-05 11:23:42 +0200

    fixing spec

--- spec/sports_live/base_spec.rb
+++ spec/sports_live/base_spec.rb
@@ -2,10 +2,6 @@ require_relative "../spec_helper.rb"
 
 describe SportsLive::Base do
 
-  it "#call should make a http get call to the required url" do
-   expect(SportsLive::Base.new.call).to eq 1
-  end
-
   it "#log should log the requests and exeptions" do
     pending
   end
